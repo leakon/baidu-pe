01fa6876dc457dbcf2faa7dfed54cdb3b7be6cac
leakon <leakon@ce5bf90a-893a-0410-9a61-e1d9927bd6ba>
2007-09-20 04:02:00 +0000

    Baidu PE Index IP Stat

---
+++ expo/stat/ipstat/tmp.pl
@@ -0,0 +1,36 @@
+require 'require.pl';
+
+#print $ENV{'PWD'} ;
+
+#array_dump(\@INC);
+
+
+
+#findSameBeginIP();
+
+
+#my %third	= ("desc" => "the third level");
+
+#my %first	= ("name" => "llk", "age" => 24, "o" => \%third);
+#my %second	= ("name" => "ffe", "age" => 14);
+
+#my %hh	= (
+#	"len"	=> 2,
+#	"first" => \%first,
+#	"second" => \%second,
+#	);
+
+#hash_dump($hh{"first"});
+#hash_dump_r(\%hh);
+
+#my @dd = getUILogFile();
+
+#array_dump(\@dd);
+
+#my @locals	= localtime;
+
+
+#array_dump(\@locals);
+
+#print date('Ymd H:i:s', time - 86400);
+
